f3a644e9190269a6380509ba54e8bf3af95cb8ac
Abhishek2122-star <abhisheklad963@gmail.com>
2026-03-23 22:47:34 +0530

    Initial commit

---
+++ StudentManagementSystem/src/test/java/com/student/management/StudentManagementSystem/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.management.StudentManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
